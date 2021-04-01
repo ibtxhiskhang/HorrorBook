a4d11945df2ef370dc61df1ce78e09a055ddeb62
Ibtxhis Khang <79154883+ibtxhiskhang@users.noreply.github.com>
2021-04-01 17:10:06 -0400

    Delete Angular Project directory

--- Angular Project/ClassRanking/ranking/src/app/students.ts
+++
@@ -1,7 +0,0 @@
-export interface Student {
-    ranking: number;
-    name: string;
-    age: number;
-    feedback: string;
-
-}
--- Angular Project/pet/src/Pets.ts
+++
@@ -1,6 +0,0 @@
-export interface Pets {
-    name: string;
-    age: number;
-    animal: string;
-    Pounds: number;
-}
